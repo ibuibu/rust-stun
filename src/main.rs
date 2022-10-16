2f08dc482d17d985730c04ca570ffef305148be2
ibuibu <ibuibu69@gmail.com>
2022-10-16 18:54:31 +0900

    remove logger / move function position

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,16 @@
 use std::env;
-#[macro_use]
-extern crate log;
 
 mod udp_server;
+mod util;
 
 fn main() {
-    env::set_var("RUST LOG", "debug");
-    env_logger::init();
-
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
-        error!("Invalid arguments.");
+        eprintln!("Invalid arguments.");
         std::process::exit(1);
     }
 
-    let address = &args[1];
+    let address_port = &args[1];
 
-    udp_server::serve(address).unwrap_or_else(|e| eprintln!("{}", e));
+    udp_server::serve(address_port).unwrap_or_else(|e| eprintln!("{}", e));
 }
